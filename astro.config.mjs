1429b1290c43cc96b2013f5d1313bcfda4959d4d
Franklin Amorim <franklin.amorim@teampicnic.com>
2025-02-22 10:59:04 +0100

    End of lesson 14

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+    site: 'https://astro-blog-cip.netlify.app',
+    integrations: [icon(), sitemap()],
 });
-
