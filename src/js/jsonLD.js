e95d17c09bd6ffc760fa6631c0da717a92aff7e9
Franklin Amorim <franklin.amorim@teampicnic.com>
2025-06-11 22:20:25 +0200

    Fix path

--- src/js/jsonLD.js
+++ src/js/jsonLD.js
@@ -1,10 +1,10 @@
-import siteData from "../data/siteData.json"
+import siteData from "../Data/siteData.json"
 import { slugify } from "./utils";
 
 
 export default function jsonLDGenerator({ type, post, url }) {
-    if (type === 'post') {
-        return `<script type="application/ld+json">
+  if (type === 'post') {
+    return `<script type="application/ld+json">
       {
         "@context": "https://schema.org",
         "@type": "BlogPosting",
@@ -23,8 +23,8 @@ export default function jsonLDGenerator({ type, post, url }) {
         "datePublished": "${post.date}"
       }
     </script>`;
-    }
-    return `<script type="application/ld+json">
+  }
+  return `<script type="application/ld+json">
       {
       "@context": "https://schema.org/",
       "@type": "WebSite",
